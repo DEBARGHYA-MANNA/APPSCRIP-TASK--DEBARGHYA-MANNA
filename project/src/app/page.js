e218ae87201e9b2bed00d2b488b95b1c7db7ad82
DEBARGHYA-MANNA <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>
2025-04-13 17:10:07 +0530

    final commit

---
+++ project/src/app/page.js
@@ -0,0 +1,15 @@
+import Footer from "./footer/page";
+import Navbar from "./nav/page";
+import Products from "./products/page";
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Products />
+      <Footer />
+    </main>
+  );
+}
